038f506827be6756ddb3e9afc0aa499326fedb60
unknown <icoul35@gmail.com>
2020-06-26 11:33:17 +0900

    update css in main page

--- frontend/src/components/main/mainInfo/MainInfo.css.js
+++ frontend/src/components/main/mainInfo/MainInfo.css.js
@@ -7,13 +7,91 @@ export const MainInfoContainer = styled.div`
   font-size: calc(2vmin);
   color: white;
   text-align: center;
-  display: flex;
   align-items: center;
+  display: flex;
+  flex-direction: column;
   overflow: hidden;
-  justify-content: center;
+  justify-content: flex-start;
+
+  & > .info-title {
+    width: 100%;
+    height: 20vh;
+    background: #111a33;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3vh;
+    margin: 20px 0px;
+
+    & > table > tr > td {
+      text-align: right;
+    }
+
+    & > table > tr > td:nth-child(2) {
+      padding-left: 2rem;
+      text-align: left;
+    }
+  }
+
+  & > .info-contents {
+    width: 100%;
+    height: 60vh;
+    background: #111a33;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 3vh;
+  }
+
+  & > .info-contents > .info-contents-title {
+    width: 100%;
+    height: 10%;
+    background: #3f3f3f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & > .info-contents > .info-contents-container {
+    width: 100%;
+    height: 80%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 
   ${props =>
     props.displayMenuName === 'icon' && (props.alignOption === 'vertical' || props.alignOption === 'area') &&
     css`justify-content: flex-start;`
+  }
+`
+
+export const MainInfoContent = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  
+  & > div {
+    height: 16.5%;
+    border: solid 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    ${props =>
+      props.status === '0' && css`background: #50bb5b;`
+    }
+
+    ${props =>
+      props.status === '1' && css`background: #e8ad2e;`
+    }
+
+    ${props =>
+      props.status === '2' && css`background: #ff0018;`
+    }
   }
 `
--- frontend/src/components/main/mainInfo/MainInfo.js
+++ frontend/src/components/main/mainInfo/MainInfo.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { useDispatch } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainInfoContainer } from './MainInfo.css.js';
+import { MainInfoContainer, MainInfoContent } from './MainInfo.css.js';
 import './MainInfo.css';
+import moment from 'moment';
 
 const MainInfo = ( props ) => {
   const dispatch = useDispatch();
@@ -22,20 +23,63 @@ const MainInfo = ( props ) => {
   return (
     <>
       <MainInfoContainer>
-        <div>현황</div>
-        <div>
-          <div>Room별 현황</div>
-          <div>
-            <div>
+        <div className="info-title">
+          <table>
+            <tr>
+              <td>기준일시 : </td>
+              <td>{moment().format('YYYY-MM-DD HH시 기준')}</td>
+            </tr>
+            <tr>
+              <td>입소현황 : </td>
+              <td>현 13명 (누계 총 111명)</td>
+            </tr>
+            <tr>
+              <td>퇴소현황 : </td>
+              <td>누계 총 30명</td>
+            </tr>
+            <tr>
+              <td>입소자 상태 종합 : </td>
+              <td>총 13명 중 정상 10명 요주의 2명 이상 1명</td>
+            </tr>
+          </table>
+        </div>
+        <div className="info-contents">
+          <div className="info-contents-title">Room별 현황</div>
+          <div className="info-contents-container">
+            <MainInfoContent status="0">
               <div>101호</div>
               <div>성명</div>
               <div>입소일</div>
               <div>0일차</div>
               <div>퇴소예정일</div>
               <div>배터리잔량</div>
-            </div>
+            </MainInfoContent>
+            <MainInfoContent status="0">
+              <div>101호</div>
+              <div>성명</div>
+              <div>입소일</div>
+              <div>0일차</div>
+              <div>퇴소예정일</div>
+              <div>배터리잔량</div>
+            </MainInfoContent>
+            <MainInfoContent status="1">
+              <div>101호</div>
+              <div>성명</div>
+              <div>입소일</div>
+              <div>0일차</div>
+              <div>퇴소예정일</div>
+              <div>배터리잔량</div>
+            </MainInfoContent>
+            <MainInfoContent status="2">
+              <div>101호</div>
+              <div>성명</div>
+              <div>입소일</div>
+              <div>0일차</div>
+              <div>퇴소예정일</div>
+              <div>배터리잔량</div>
+            </MainInfoContent>
           </div>
-          <div>ㅇㅇㅇ</div>
+          <div>대한민국을 건강하게 지키는 힘!</div>
         </div>
       </MainInfoContainer>
     </>
